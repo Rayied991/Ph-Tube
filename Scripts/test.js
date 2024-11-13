33bf23eb27ee55c56fab320440ade25d403e33e6
RRayied991 <hussainrayied9@gmail.com>
2024-11-13 08:51:04 +0600

    code pushed

---
+++ Scripts/test.js
@@ -0,0 +1,19 @@
+// // const isVerified="";
+// // // if(isVerified){
+// // //     console.log("Verified");
+// // // }else{
+// // //     console.log("Not Verified");
+// // // }
+// // console.log(`${isVerified === true? "Verified":"Not Verified"}`);
+// function getTimestring(time){
+//     //get hour and rest seconds
+//     const hour=parseInt(time/3600);//get hour
+//     let remainingSeconds=time%3600;//get remaining seconds
+//     const minute=parseInt(remainingSeconds/60);//get minute
+//     remainingSeconds=remainingSeconds%60;
+//     //get year and month
+//     const year=parseInt(time/365);
+//     return `${year} year ${hour} hour ${minute} minute ${remainingSeconds} second ago`;
+
+// }
+// console.log(getTimestring(7865));
